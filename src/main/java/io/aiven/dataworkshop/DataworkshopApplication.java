c8ec5a03d739d3f8f429df77280fd01c6682b4f8
Sébastien Blanc <scm.blanc@gmail.com>
2023-10-24 09:02:54 +0200

    initial commit

---
+++ src/main/java/io/aiven/dataworkshop/DataworkshopApplication.java
@@ -0,0 +1,13 @@
+package io.aiven.dataworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataworkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataworkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/aiven/dataworkshop/DataworkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package io.aiven.dataworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataworkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
